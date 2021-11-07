6c44d3d6f26ddb499409b80a3b566877ad558eb5
Joey Goertzen <joey.goertzen@gmail.com>
2021-11-06 23:38:28 -0600

    get movement working

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { io, Socket } from 'socket.io-client';
+import { Position } from './position';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'client';
+
+  socket: Socket;
+  position: Position;
+
+  constructor() {
+    this.position = new Position(0, 0);
+    
+    const socket = io('http://localhost:3000');
+    
+    this.socket = socket;
+    
+    socket.on("connect", () => {
+      console.log('connected');
+    });
+  }
+
+  @HostListener('document:keydown', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    switch(event.code) {
+      case 'KeyD': this.position.x += 1; break;
+      case 'KeyA': this.position.x -= 1; break;
+      case 'KeyS': this.position.y += 1; break;
+      case 'KeyW': this.position.y -= 1; break;
+    }
+  }
 }
--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 import { Server } from 'socket.io';
 
-const io = new Server({});
+const io = new Server({
+    serveClient: false,
+    cors: {
+        origin: "http://localhost:4200"
+    }
+});
 
 io.on("connection", socket => {
-    console.log('got a connection!', socket);
+    console.log('got a connection!', socket.id);
 });
 
 io.listen(3000);
-
-console.log('server is up');
